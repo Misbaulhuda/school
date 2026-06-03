a56ae97f23f471146fc3a35a524bee87476e9d31
Misbaulhuda <misbahuda912@gmail.com>
2026-06-03 16:19:47 +0530

    Update analysis.py

--- notebook/analysis.py
+++ notebook/analysis.py
@@ -5,3 +5,10 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+os.makedirs("../visuals", exist_ok=True)
+zip_path = "../archive.zip"
+extract_path = "../data"
+os.makedirs(extract_path, exist_ok=True)
+with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+    zip_ref.extractall(extract_path)
+print("\n========== ZIP FILE EXTRACTED ==========\n")
